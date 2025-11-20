73fe2f051b5fef893334a423295be384ee1eb2f6
Carlos Vinicius <viniciusdev.26@gmail.com>
2025-11-19 21:25:31 -0300

    🎉 initial commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import { User } from "./models/user";
+import mongoose from "mongoose";
+
+const app = express();
+
+await mongoose.connect("mongodb://root:root@localhost:27017/no-sql-project?authSource=admin");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the NoSQL Project API");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  const newUser = new User(req.body);
+  await newUser.save();
+  res.status(201).json(newUser);
+});
+
+app.put("/users/:id", async (req, res) => {
+  const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedUser);
+});
+
+app.delete("/users/:id", async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.status(204).send();
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
